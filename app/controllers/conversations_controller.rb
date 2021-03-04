fd77222ab944e8b06035d117cfe20a0c0eafcad0
Kevin Jung <kev.jung.ii@gmail.com>
2021-03-04 17:19:59 -0500

    adds notes to routes in messages to help with future code

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,36 +1,36 @@
 class ConversationsController < ApplicationController
 
-  # GET: /conversations
+  # GET: /conversations -lists all current user conversations
   get "/conversations" do
     erb :"/conversations/index.html"
   end
 
-  # GET: /conversations/new
+  # GET: /conversations/new -shows form to create a new conversation with the new user and selected friends
   get "/conversations/new" do
     erb :"/conversations/new.html"
   end
 
-  # POST: /conversations
+  # POST: /conversations -posts the new conversation from /conversations/new
   post "/conversations" do
     redirect "/conversations"
   end
 
-  # GET: /conversations/5
+  # GET: /conversations/5 -shows all the messages in a given conversation
   get "/conversations/:id" do
     erb :"/conversations/show.html"
   end
 
-  # GET: /conversations/5/edit
+  # GET: /conversations/5/edit -shows the form to allow removal or addition of users to a conversation
   get "/conversations/:id/edit" do
     erb :"/conversations/edit.html"
   end
 
-  # PATCH: /conversations/5
+  # PATCH: /conversations/5 -patches the data from /conversations/:id/edit
   patch "/conversations/:id" do
     redirect "/conversations/:id"
   end
 
-  # DELETE: /conversations/5/delete
+  # DELETE: /conversations/5/delete - removes a conversation(must be in the only user in conversation to delete it --otherwise should just edit and remove current user--)
   delete "/conversations/:id/delete" do
     redirect "/conversations"
   end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,36 +1,26 @@
 class MessagesController < ApplicationController
 
-  # GET: /messages
-  get "/messages" do
-    erb :"/messages/index.html"
-  end
-
-  # GET: /messages/new
+  # GET: /messages/new -sends a new message
   get "/messages/new" do
     erb :"/messages/new.html"
   end
 
-  # POST: /messages
+  # POST: /messages -posts new message created in /messages/new
   post "/messages" do
     redirect "/messages"
   end
 
-  # GET: /messages/5
-  get "/messages/:id" do
-    erb :"/messages/show.html"
-  end
-
-  # GET: /messages/5/edit
+  # GET: /messages/5/edit -allows the user to edit message they have already sent
   get "/messages/:id/edit" do
     erb :"/messages/edit.html"
   end
 
-  # PATCH: /messages/5
+  # PATCH: /messages/5 -patches changes from /messages/:id/edit
   patch "/messages/:id" do
     redirect "/messages/:id"
   end
 
-  # DELETE: /messages/5/delete
+  # DELETE: /messages/5/delete -deletes a message (must be user that sent message to delete --possibly add the ability for conversation admins and let them delete as well)
   delete "/messages/:id/delete" do
     redirect "/messages"
   end
