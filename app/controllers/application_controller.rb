3979b61c78c8942c8dbb9d3f9ec595d90bf3f06c
Kevin Jung <kev.jung.ii@gmail.com>
2021-03-04 16:46:38 -0500

    adds notes to user routes to guide future code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,6 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
-  get '/signup' do
-    erb :'users/new'
-  end
-
   get '/login' do
     erb :'login'
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,36 @@
 class UsersController < ApplicationController
 
-  # GET: /users
+  # GET: /users -friends page
   get "/users" do
     erb :"/users/index.html"
   end
 
-  # GET: /users/new
+  # GET: /users/new -also known as signup
   get "/users/new" do
     erb :"/users/new.html"
   end
 
-  # POST: /users
+  # POST: /users -posts results from /users/new
   post "/users" do
     redirect "/users"
   end
 
-  # GET: /users/5
+  # GET: /users/5 -profile pages
   get "/users/:id" do
     erb :"/users/show.html"
   end
 
-  # GET: /users/5/edit
+  # GET: /users/5/edit -edit profile
   get "/users/:id/edit" do
     erb :"/users/edit.html"
   end
 
-  # PATCH: /users/5
+  # PATCH: /users/5 -patches changes made to a profile from /users/:id/edit
   patch "/users/:id" do
     redirect "/users/:id"
   end
 
-  # DELETE: /users/5/delete
+  # DELETE: /users/5/delete - removes a user(can only be done by the logged in user)
   delete "/users/:id/delete" do
     redirect "/users"
   end
