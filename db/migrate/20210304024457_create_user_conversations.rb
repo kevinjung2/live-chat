8f2750a0d7996754adae03dc8015d351e4e5d2fe
Kevin Jung <kev.jung.ii@gmail.com>
2021-03-03 21:47:41 -0500

    creates the user_conversation table

--- db/migrate/20210304024457_create_user_conversations.rb
+++ db/migrate/20210304024457_create_user_conversations.rb
@@ -1,7 +1,8 @@
 class CreateUserConversations < ActiveRecord::Migration
   def change
     create_table :user_conversations do |t|
-
+      t.integer :follwer_id
+      t.integer :followed_id
       t.timestamps null: false
     end
   end
