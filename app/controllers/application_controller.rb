44e2350a7e686f24d69822f390e4a0464f192c09
Kevin Jung <kev.jung.ii@gmail.com>
2021-03-04 17:43:32 -0500

    adds functionality to post '/login'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "46d9f485d124b5091ec14c978048e267fad3ceea0e9968e14e56474f0b69417643a453a4e964c4190f18b4cb74fe1cbe9a65c1ca37bd2549af7eb006aea7fbcd"
   end
 
   get "/" do
@@ -13,6 +15,20 @@ class ApplicationController < Sinatra::Base
 
   get '/login' do
     erb :'login'
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if !user
+      flash[:message] = "The username #{params[:username]} was not found. Enter a different username of Sign up instead!"
+      redirect :'/login'
+    elsif user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect :"/users/#{user.id}"
+    else
+      flash[:message] = "Incorrect password for the username #{params[:username]}"
+      redirect :'/login'
+    end
   end
 
   get '/logout' do
