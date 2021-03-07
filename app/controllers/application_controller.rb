cc98815f22357367eeac367219dc4903a7f56052
Kevin Jung <kev.jung.ii@gmail.com>
2021-03-06 22:43:09 -0500

    makes flash messages show on login and signup pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     user = User.find_by(username: params[:username])
     if !user
-      flash[:message] = "The username #{params[:username]} was not found. Enter a different username of Sign up instead!"
+      flash[:message] = "The username #{params[:username]} was not found. Enter a different username or Sign up instead!"
       redirect :'/login'
     elsif user.authenticate(params[:password])
       session[:user_id] = user.id
