20043863768d2dd800fc19bfbca3617d56548b88
Kevin Jung <kev.jung.ii@gmail.com>
2021-03-17 07:46:37 -0400

    adds a description to the user class to make explain self join table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   has_many :messages
   has_many :user_conversations
   has_many :conversations, through: :user_conversations
+  #self join table makes it so users can be assossiated with other users
+  #sets the method for setting and retrieving a users followers from the friendship join table to the follower_id foreign key in the friendship table.
+  #does the same for a users "followed" users with the followed_id foreign key.
   has_many :followers, foreign_key: :follower_id , class_name: "Friendship"
   has_many :followed, through: :followers
   has_many :followed, foreign_key: :followed_id, class_name: "Friendship"
